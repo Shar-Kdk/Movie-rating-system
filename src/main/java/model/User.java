2df409c9b40d19763be77d8a4251f38fdd3632d2
Shar-Kdk <np05cp4a230249@iic.edu.np>
2025-04-14 23:17:24 +0545

    created user module

---
+++ src/main/java/model/User.java
@@ -0,0 +1,77 @@
+import java.util.Date;
+
+public class User {
+
+    private int userId;
+    private String username;
+    private String email;
+    private String password;
+    private Date registerDate;
+    private int roleId;
+    private String userAvatar;
+
+    public User(int userId, String username, String email, String password, Date registerDate, int roleId, String userAvatar) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.registerDate = registerDate;
+        this.roleId = roleId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getUserAvatar() {
+        return userAvatar;
+    }
+
+    public void setUserAvatar(String userAvatar) {
+        this.userAvatar = userAvatar;
+    }
+}
