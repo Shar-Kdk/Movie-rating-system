8ebe4847703877833d82838bf9b17bb9c45e3f8c
Jenish17 <jstha9919@gmail.com>
2025-04-15 09:22:32 +0545

    Added MovieGenre Model Class

---
+++ src/main/java/model/MovieGenre.java
@@ -0,0 +1,25 @@
+public class MovieGenre {
+    private int movieId;
+    private int genreId;
+
+    public MovieGenre(int movieId, int genreId) {
+        this.movieId = movieId;
+        this.genreId = genreId;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+}
